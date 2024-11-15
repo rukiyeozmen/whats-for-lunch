80fa8f7e245eed21bb1daea5db420baa96b0eaf8
Rukiye <rukiiye42@gmail.com>
2024-11-16 00:05:51 +0300

    modified lunch.js file

--- lunch.js
+++ lunch.js
@@ -14,7 +14,17 @@
  */
 
 const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
+if(!hungry){
+  return 'get back to work'
+}
+if(availableTime < 20 ){
+  return 'pick something up and eat it in the lab'
+}
+if (availableTime >= 20 && availableTime <= 30){
+  return 'you deserve a break and should take time to cook a tasty meal'
+}
+return 'this is an intense program after all and you should probably reconsider'
+  
 }
 
 
@@ -24,16 +34,16 @@ const whatToDoForLunch = function(hungry, availableTime) {
  */
 
 console.log("I'm hungry and I have 20 minutes for lunch.");
-whatToDoForLunch(true, 20);
+console.log(whatToDoForLunch(true, 20));
 console.log("---");
 
 console.log("I'm hungry and I have 50 minutes for lunch.");
-whatToDoForLunch(true, 50);
+console.log(whatToDoForLunch(true, 50));
 console.log("---");
 
 console.log("I'm not hungry and I have 30 minutes for lunch.");
-whatToDoForLunch(false, 30);
+console.log(whatToDoForLunch(false, 30));
 console.log("---");
 
 console.log("I'm hungry and I have 15 minutes for lunch.");
-whatToDoForLunch(true, 15);
+console.log(whatToDoForLunch(true, 15));
